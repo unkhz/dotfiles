f71d0bf9df0a9f07ad36d4c2cb3edd5f385194fc
Juhani Pelli <juhani.pelli@sc5.io>
2014-10-10 14:29:42 +0300

    Add .bashrc

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ dotfiles="$(dirname $0)"
 
 ln -s $dotfiles/.vimrc ~/.vimrc
 ln -s $dotfiles/.vim ~/.vim
+ln -s $dotfiles/.bashrc ~/.bashrc
 ln -s $dotfiles/.profile ~/.profile
 ln -s $dotfiles/.path ~/.path
