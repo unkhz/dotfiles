54bf98be50b3c44c49ca1be7d1c2ef897b42aef5
Juhani Pelli <juhani.pelli@gmail.com>
2014-10-22 21:29:12 +0300

    Add keys

--- install.sh
+++ install.sh
@@ -8,3 +8,12 @@ ln -s $dotfiles/.bashrc ~/.bashrc
 ln -s $dotfiles/.profile ~/.profile
 ln -s $dotfiles/.path ~/.path
 ln -s $dotfiles/.slate ~/.slate
+
+if [ -f ~/.ssh/authorized_keys ]; then
+  cat ~/.ssh/authorized_keys $dotfiles/.ssh/authorized_keys | uniq > ~/.ssh/authorized_keys
+fi
+
+if [ -d ~/Library ]; then
+  cp -a $dotfiles/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/
+  cp -a $dotfiles/Library/Keyboard\ Layouts/fi-unkhz-mac.keylayout ~/Library/Keyboard\ Layouts/
+fi
