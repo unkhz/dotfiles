519a24f8185fa70392a8b0ca646b5a03285d479f
Juhani Pelli <juhani.pelli@gmail.com>
2020-11-09 13:41:16 +0200

    Fix typo

--- install-xkb.sh
+++ install-xkb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo rm -f /usr/share/X11/xkb/symbols/fi
-sudo ln -s /home/unkhz/xkb/symbols/unkhz /usr/share/X11/xkb/symbols/fi
+sudo ln -s /home/unkhz/.dotfiles/xkb/symbols/unkhz /usr/share/X11/xkb/symbols/fi
