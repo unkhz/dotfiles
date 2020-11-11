52ca0e6c1e589d2cd93ef1ce479749b4fcc3b2a8
Juhani Pelli <juhani.pelli@gmail.com>
2020-11-11 23:23:41 +0200

    Install console keymap

--- install-kb.sh
+++ install-kb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# console
+localectl set-keymap --no-convert /home/unkhz/.dotfiles/xkb.kbd/unkhz.map
+
+# xorg
+sudo mkdir -p /usr/share/X11/xkb/symbols
 sudo rm -f /usr/share/X11/xkb/symbols/fi
 sudo ln -s /home/unkhz/.dotfiles/xkb/symbols/unkhz /usr/share/X11/xkb/symbols/fi
