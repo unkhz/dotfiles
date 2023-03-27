64782b91e15ee155630c25ee5c7e99cec001f4e0
Juhani Pelli <juhani.pelli@gmail.com>
2023-03-27 11:09:07 +0300

    Add shortcut for moving to next screen

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -10,8 +10,17 @@ local function resizeWindow(xr, wr)
   win:setFrame(f, 0)
 end
 
+local function moveToNextScreen()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end
+
 local modifiers = {"cmd", "ctrl"}
 
 hs.hotkey.bind(modifiers, "up", function() resizeWindow(0, 1) end)
+hs.hotkey.bind(modifiers, "down", function() moveToNextScreen() end)
 hs.hotkey.bind(modifiers, "left", function() resizeWindow(0, 0.5) end)
 hs.hotkey.bind(modifiers, "right", function() resizeWindow(0.5, 0.5) end)
