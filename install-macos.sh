ce3e49466396a083aeb05b3424b51888fe8d3b72
Juhani Pelli <juhani.pelli@gmail.com>
2024-07-18 14:18:13 +0300

    Split OS setups to differenct scripts

---
+++ install-macos.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+if [ -d ~/Library ]; then
+  echo "🚀 Installing nvm"
+  if [ ! -d ~/.nvm ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  fi
+
+  echo "🚀 Installing configurations"
+  mkdir -p ~/Library/Application\ Support/lazygit
+  ln $lnopts $dotfiles/.lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+
+  ln $lnopts $dotfiles/.slate ~
+
+  ln $lnopts $dotfiles/.hammerspoon ~
+
+  mkdir -p ~/.config/karabiner/assets/complex_modifications
+  ln $lnopts $dotfiles/.config/karabiner/assets/complex_modifications/unkhz.json ~/.config/karabiner/assets/complex_modifications/unkhz.json
+
+  # Make fn-left and fn-right properly jump to start and end of line
+  mkdir -p ~/Library/KeyBindings
+  cp -a $dotfiles/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/
+
+  # Add Fi-Unkhz programming layout
+  cp -a $dotfiles/Library/Keyboard\ Layouts/fi-unkhz-mac.keylayout ~/Library/Keyboard\ Layouts/
+
+  # Repair keyhold feature
+  defaults write -g ApplePressAndHoldEnabled -bool false
+
+  echo "🚀 Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo "🚀 Installing lazygit"
+  brew install lazygit
+
+  echo "🚀 Installing git-delta"
+  brew install git-delta
+  git config --global core.pager delta
+else
+  echo "❓ This does not seem to be MacOS. Are you sure you're running the correct thing?"
+fi
--- install.sh
+++ install.sh
@@ -22,11 +22,6 @@ if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-# nvm
-if [ ! -d ~/.nvm ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-fi
-
 ln $lnopts $dotfiles/.vimrc ~
 ln $lnopts $dotfiles/.vim ~
 ln $lnopts $dotfiles/.bashrc ~
@@ -36,36 +31,8 @@ ln $lnopts $dotfiles/.path ~
 ln $lnopts $dotfiles/bin ~
 ln $lnopts $dotfiles/.oh-my-zsh/custom/themes/unkhz.zsh-theme ~/.oh-my-zsh/custom/themes
 
-if [ -d ~/Library ]; then
-  mkdir -p ~/Library/Application\ Support/lazygit
-  ln $lnopts $dotfiles/.lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
-
-  ln $lnopts $dotfiles/.slate ~
-
-  ln $lnopts $dotfiles/.hammerspoon ~
-
-  mkdir -p ~/.config/karabiner/assets/complex_modifications
-  ln $lnopts $dotfiles/.config/karabiner/assets/complex_modifications/unkhz.json ~/.config/karabiner/assets/complex_modifications/unkhz.json
-
-  # Make fn-left and fn-right properly jump to start and end of line
-  mkdir -p ~/Library/KeyBindings
-  cp -a $dotfiles/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/
-
-  # Add Fi-Unkhz programming layout
-  cp -a $dotfiles/Library/Keyboard\ Layouts/fi-unkhz-mac.keylayout ~/Library/Keyboard\ Layouts/
-
-  # Repair keyhold feature
-  defaults write -g ApplePressAndHoldEnabled -bool false
-
-  # Install Homebrew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  # Install Homebrew packages
-  brew install lazygit
-  brew install git-delta
-fi
-
 # Git settings
 git config --global user.name "Juhani Pelli"
 git config --global user.email "juhani.pelli@gmail.com"
-git config --global core.pager delta
+git config --global core.editor vim
+git config --global pull.rebase true
